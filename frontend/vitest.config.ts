8b1d6902235cd0013090d6f586ba4e3787c8f62c
idaballegaard <idabal01@easv365.dk>
2026-05-30 08:27:38 +0200

    Revert "tests: set up Vitest unit tests for backend and frontend"
    This reverts commit f7763b48f0dff013d89cf65c626c18b42acff9f9.

--- frontend/vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vitest/config";
-
-export default defineConfig({
-  test: {
-    include: ["src/**/*.unit.test.ts"],
-    environment: "jsdom",
-    globals: true,
-  },
-});
